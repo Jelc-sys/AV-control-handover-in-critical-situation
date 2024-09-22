313a837a14a7ba797a73e1b5d215fe523a62ba76
luca-mifsud <lucamifbuh@gmail.com>
2024-09-22 12:34:12 +0200

    Dashboard Countdown
    Dashboard will now display a 5 second countdown before showing the 'Warning' symbol. Reaction time reading will also be started.

--- Assets/Scripts/eHMI/SpriteHMI_DASH.cs
+++ Assets/Scripts/eHMI/SpriteHMI_DASH.cs
@@ -5,6 +5,7 @@ using UnityEngine.Rendering.PostProcessing;
 using UnityEngine.UI;
 using UnityEngine.Rendering;
 using System.Threading;
+using System.Threading.Tasks;
 
 //simple sprite swapping implementation of HMI base class
 public class SpriteHMI_DASH : HMI
@@ -12,13 +13,27 @@ public class SpriteHMI_DASH : HMI
     [SerializeField]
     public SpriteRenderer _renderer_Dash;
     [SerializeField]
+    Sprite disp5;
+    [SerializeField]
+    Sprite disp4;
+    [SerializeField]
+    Sprite disp3;
+    [SerializeField]
+    Sprite disp2;
+    [SerializeField]
+    Sprite disp1;
+    [SerializeField]
     Sprite dash;
     [SerializeField]
 	Sprite disabled;
 
-    public override void Display(HMIState state)
+    public GameObject reactionTimeTracker;
+
+    public override async void Display(HMIState state)
     {
         base.Display(state);
+        reactionTimeTracker = GameObject.Find("ReactionTimeTracker");
+        ReactionTimeTracker reactionScript = reactionTimeTracker.GetComponent<ReactionTimeTracker>();
 
         if (_renderer_Dash == null)
         {
@@ -29,6 +44,23 @@ public class SpriteHMI_DASH : HMI
         switch (state)
         {
             case HMIState.DASH:
+                //startTime = Time.time;
+                spr = disp5;
+                _renderer_Dash.sprite = spr;
+                reactionScript.StartTimer("Dash countdown");
+                await WaitForSecondsAsync(1f);
+                spr = disp4;
+                _renderer_Dash.sprite = spr;
+                await WaitForSecondsAsync(2f);
+                spr = disp3;
+                _renderer_Dash.sprite = spr;
+                await WaitForSecondsAsync(2f);
+                spr = disp2;
+                _renderer_Dash.sprite = spr;
+                await WaitForSecondsAsync(2f);
+                spr = disp1;
+                _renderer_Dash.sprite = spr;
+                await WaitForSecondsAsync(2f);
                 spr = dash;
                 break;
             default:
@@ -37,6 +69,10 @@ public class SpriteHMI_DASH : HMI
         }
         _renderer_Dash.sprite = spr;
 
+    }
+    private async Task WaitForSecondsAsync(float seconds)
+    {
+        await Task.Delay((int)(seconds * 1000)); // Convert seconds to milliseconds
     }
 }
 
